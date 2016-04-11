b6b22672a8d129f9ddd6c1eac18336583d6943b5
aaa <aaa@ubuntu.(none)>
2016-04-11 04:46:28 -0700

    añadido view y link para borrar post

--- blog/views.py
+++ blog/views.py
@@ -50,3 +50,8 @@ def post_edit(request, pk):
         else:
             form = PostForm(instance=post)
         return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_delete(request,pk):
+        post = get_object_or_404(Post, pk=pk)
+        post.delete()
+        return redirect('blog.views.post_list')
