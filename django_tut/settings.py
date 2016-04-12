08acedbd020203861f3c0b7ca06a21443db74514
aaa <aaa@ubuntu.(none)>
2016-04-12 03:12:55 -0700

    cambios en master

--- django_tut/settings.py
+++ django_tut/settings.py
@@ -14,6 +14,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_DIR = os.path.join(BASE_DIR,'django_tut')
 
 
 # Quick-start development settings - unsuitable for production
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'django_tut.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(PROJECT_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
