961a232df9d40355aa8ab969bd97bbf6919991bf
aaa <aaa@ubuntu.(none)>
2016-04-18 07:38:39 -0700

    hol

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 urlpatterns = [
         url(r'^$', views.PostList.as_view(),name='post_list'),
         url(r'^post/(?P<pk>[0-9]+)/$', views.PostDetail.as_view(),name='post_detail'),
-        url(r'^post/new/$',login_required(views.PostCreate,login_url='login'), name='post_new'),
+        url(r'^post/new/$',login_required(views.PostCreate.as_view(),login_url='login'), name='post_new'),
         url(r'^post/(?P<pk>[0-9]+)/edit/$', views.PostEdit.as_view(), name='post_edit'),
         url(r'^post/(?P<pk>[0-9]+)/delete/$', views.PostDelete.as_view(), name='post_delete'),
     ]
