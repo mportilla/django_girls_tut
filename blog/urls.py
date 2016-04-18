c34dd2ae4d82fcd8ac13b0c344c0e61b85a9a30f
aaa <aaa@ubuntu.(none)>
2016-04-18 07:25:29 -0700

    hol

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import include, url
 from . import views
 from django.contrib.auth.decorators import login_required
-from django.views.generic import TemplateView
+
 
 urlpatterns = [
         url(r'^$', views.PostList.as_view(),name='post_list'),
